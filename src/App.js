6a5bce1c1ebcfa415396c8ce2d52e54f40dd7691
Siddhesh Surve <sysurve24@gmail.com>
2023-07-22 10:19:08 +0000

    Body started

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Body from './components/Body/Body';
 import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Body />
     </div>
   );
 }
---
+++ src/components/Body/Body.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { ArrowDown } from "react-feather";
+import styles from "./Body.module.css";
+
+const Body = () => {
+  return (
+    <div className={styles.container}>
+      <p className={styles.heading}>Resume Builder</p>
+    </div>
+  );
+};
+
+export default Body;
